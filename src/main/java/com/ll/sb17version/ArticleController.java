1d7c3df8495fca0e2f341b1207aaec4b8a4ed717
dongha922 <kdh4575@naver.com>
2023-11-15 03:30:45 +0900

    ãarticle객체 생성 && ãwrite.ãhtml 재창조

--- src/main/java/com/ll/sb17version/ArticleController.java
+++ src/main/java/com/ll/sb17version/ArticleController.java
@@ -1,5 +1,7 @@
 package com.ll.sb17version;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,10 +15,21 @@ public class ArticleController {
     }
     @GetMapping("/article/doWrite")
     @ResponseBody
-    String doWrite(
+    Article doWrite(
             String title,
             String body
     ) {
-        return "게시물이 작성되었습니다.";
+        //객체만들기 객체화
+        Article article = new Article(1,title, body);
+        return article;
     }
+}
+@AllArgsConstructor
+//getter 은 getid ,gettitle, getBody 함수 자동완성
+@Getter
+
+class Article{
+    private long id;
+    private String title;
+    private String body;
 }
