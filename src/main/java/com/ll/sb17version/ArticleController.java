3770f2404caa6e83ab7b4d688737651f29d2a56a
dongha922 <kdh4575@naver.com>
2023-11-15 03:49:28 +0900

    msg, dstep5 µ

--- src/main/java/com/ll/sb17version/ArticleController.java
+++ src/main/java/com/ll/sb17version/ArticleController.java
@@ -11,6 +11,8 @@ import java.util.Map;
 
 @Controller
 public class ArticleController {
+    //전역변수 선언
+    private Article lastArticle;
     @GetMapping("/article/write")
     String showWrite(){
 
@@ -18,17 +20,23 @@ public class ArticleController {
     }
     @GetMapping("/article/doWrite")
     @ResponseBody
+
     Map<String, Object> doWrite(
             String title,
             String body
     ) {
         //객체만들기 객체화
-        Article article = new Article(1,title, body);
+        lastArticle = new Article(1,title, body);
 
         Map<String, Object> rs= new HashMap<>();
         rs.put("msg", "1번 게시물작성됨");
-        rs.put("data", article);
+        rs.put("data", lastArticle);
          return rs;
+    }
+    @GetMapping("/article/getLastArticles")
+    @ResponseBody
+    Article getLastArticle(){
+        return lastArticle;
     }
 }
 @AllArgsConstructor
